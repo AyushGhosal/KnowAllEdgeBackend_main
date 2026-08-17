41b161e90c98d92b42197458016572c99b1e3195
AyushGhosal <75989615+AyushGhosal@users.noreply.github.com>
2026-08-17 15:52:24 +0530

    test ci/cd

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const { constants } = require("./constant");
 app.get('/',(req, res)=>{
     const healthInfo = {
     appName: 'KnowAllEdge API',
-    status: 'Running',
+    status: 'Running 1.0',
     uptime: process.uptime().toFixed(2) + ' seconds',
     environment: process.env.NODE_ENV || 'development'
   };
